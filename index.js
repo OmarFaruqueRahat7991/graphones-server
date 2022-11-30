48f2d7a5a16f4e1a241f24cf9f23bbd51ebe639d
OmarFaruqueRahat7991 <rahatomarfaruque@gmail.com>
2022-11-30 22:26:34 +0600

    another commit

--- index.js
+++ index.js
@@ -16,6 +16,20 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ypme9qn.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+async function run() {
+    try {
+        const allBrandsCollection = client.db('grabPhones').collection('usedMobilesCollection')
+
+        
+
+    }
+    finally {
+
+    }
+
+}
+run().catch(console.log());
+
 
 app.get('/', async (req, res) => {
     res.send('My Twelfth Assignment Server Is Running');
