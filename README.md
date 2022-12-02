4c6d695c181c0dfa15a862c7a25d4ba7af154f61
OmarFaruqueRahat7991 <rahatomarfaruque@gmail.com>
2022-12-02 21:29:03 +0600

    another commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# Project's Title: GrabPhones(Server Side)
+
+This project is the server side of my-twelfth-assignment of my web development course.
+
+
+
+## Technologies i Used In This Project:
+1. NodeJS
+2. Express
+3. Cors
+4. Dotenv
+5. MongoDB
+6. Vercel
+
+
+## Live Website Link: 
+## Live Website Github Repository Link: https://github.com/OmarFaruqueRahat7991/my-twelfth-assignment-client
+## Server Site Link: https://my-twelfth-assignment-server.vercel.app/
+
+
+  
+
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
@@ -13,7 +13,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ypme9qn.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ypme9qn.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run() {
@@ -30,7 +30,6 @@ async function run() {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
             const allCategories = await allBrandsCollection.find(query).toArray();
-            console.log(allCategories);
             res.send(allCategories)
         })
 
